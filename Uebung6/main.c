d577fc91158a9497dff9958cea67621b36e6fe5b
MarkG <Mark.Geiger@dlr.de>
2015-04-24 00:30:40 +0200

    added initial files for U6.1

---
+++ Uebung6/main.c
@@ -0,0 +1,17 @@
+//-------------------------------------//
+#include <stdio.h>
+#include <stdlib.h>
+#include <cblas.h>
+#include <string.h>
+#include <x86intrin.h>
+#include "timing.h"
+//-------------------------------------//
+#define FALSE   0
+#define TRUE    1
+//-------------------------------------//
+
+int main()
+{
+    return 0;
+}
+
